a347a2d9dcd044e0ade3e631df1fa568b71fca9d
AjaxMhdy-17 <mhdyhasan017@gmail.com>
2026-06-07 18:20:52 +0600

    init-commit

---
+++ backend/lrvl-adv/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::inertia('/', 'welcome')->name('home');
+
+Route::get('/cart', function () {
+    return Inertia::render('cart');
+})->name('cart');
+
+
+Route::get('/page/{parameter}', function () {
+    return Inertia::render('page');
+})->name('page');
